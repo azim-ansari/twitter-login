55a05b429f56afa1f2f31c1ac8dc18a731baf531
Azim <84502882+azim-ansari@users.noreply.github.com>
2022-04-19 14:08:02 +0530

    Update twitter.config.js

--- backend/twitter.config.js
+++ backend/twitter.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  consumerKey: "0xs9H6i2T8grDb5wgzMlxRV8T",
-  consumerSecret: "pQX4d9qjsmLAXPfZP094r9H8wliNzb4eaFDxlFlyv628N21CE1"
+  consumerKey: "00s9H6i2T8grDb5wgzMlxRV8T",
+  consumerSecret: "ppX4d9qjsmLAXPfZP094r9H8wliNzb4eaFDxlFlyv628N21CE1"
 };
